8bb559d5dd2d9a6a10706860abfd2aa9c2832f7d
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-08-17 17:50:17 -0300

    Init

--- game.c
+++ game.c
@@ -1,9 +1,11 @@
 #include <stdbool.h>
+#include <stddef.h>
 #define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
 #include "nob.h"
+#define da_foreach_rev(Type, it, da) for (Type *it = (da)->items+(da)->count-1; it > (da)->items; --it)
 #include "raylib.h"
-#define GRID_MULT 128
+#define GRID_MULT 32
 #define GRID_SIZE (1 * GRID_MULT)
 #define CELL_SIZE (960 / GRID_MULT)
 #define WINDOW_WIDTH (GRID_SIZE * CELL_SIZE)
@@ -14,10 +16,13 @@
 #define GRIDLINECOLOR BLACK
 #define GRIDLINETICKNESS 0.0f
 #define MOUSEHITBOX 0.1f
+#define SIMULATION_SPEED 10
 
 typedef enum {
     CELL_TYPE_NONE,
     CELL_TYPE_SAND,
+    CELL_TYPE_WATER,
+    CELL_TYPE_ROCK,
 } Cell_Type;
 
 typedef struct {
@@ -25,7 +30,7 @@ typedef struct {
     Color color;
 
     bool selected;
-    bool active;
+    bool updated;
     Cell_Type type;
 } Cell;
 
@@ -35,7 +40,8 @@ typedef struct {
 } Grid;
 
 void UpdateSand(Grid* grid, Cell*c, int col, int row);
-void UpdateLife(Grid* grid, Cell*c, int col, int row);
+void UpdateWater(Grid* grid, Cell*c, int col, int row);
+bool UpdateCell(Grid* grid, Cell* c, Cell_Type old_type, Cell_Type new_type, Cell_Type next_type, size_t next_pos);
 
 int main(void) {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "SandBox");
@@ -49,6 +55,7 @@ int main(void) {
     char * legend = temp_sprintf("%dx%d Grid", GRID_SIZE, GRID_SIZE);
     const int cell_size = CELL_SIZE;
     Cell_Type curr_place_type = CELL_TYPE_SAND;
+    size_t frameCounter = 0;
 
     for (int row = 0; row < GRID_SIZE; row++)
     {
@@ -61,22 +68,31 @@ int main(void) {
                     .width = cell_size,
                     .height = cell_size,
                 },
-                .color = ((row + col) % 2 == 0) ? LIGHTCOLOR : DARKCOLOR,
+                .type = (row == 0 || col == 0) || (row == GRID_SIZE-1 || col == GRID_SIZE-1)  ? CELL_TYPE_ROCK : CELL_TYPE_NONE,
+                .color = BLACK, // ((row + col) % 2 == 0) ? LIGHTCOLOR : DARKCOLOR,
             };
             da_append(&grid, cell);
         }
     }
 
     while (!WindowShouldClose()) {
-        da_foreach(Cell, it, &grid) {
-            if(!it->active) continue;
-            int col = it->rect.x / cell_size;
-            int row = (it->rect.y - UI_OFFSET) / cell_size;
-            switch (it->type) {
-                case CELL_TYPE_SAND: {
-                    UpdateSand(&grid, it, col, row);
-                } break;
-                default: break;
+        frameCounter++;
+        if (frameCounter >= SIMULATION_SPEED) {
+            frameCounter = 0;
+            da_foreach_rev(Cell, it, &grid) {
+                if(it->type == CELL_TYPE_NONE) continue;
+                if(it->type == CELL_TYPE_ROCK) continue;
+                int col = it->rect.x / cell_size;
+                int row = (it->rect.y - UI_OFFSET) / cell_size;
+                switch (it->type) {
+                    case CELL_TYPE_SAND: {
+                        UpdateSand(&grid, it, col, row);
+                    } break;
+                    case CELL_TYPE_WATER: {
+                        UpdateWater(&grid, it, col, row);
+                    } break;
+                    default: break;
+                }
             }
         }
 
@@ -86,16 +102,18 @@ int main(void) {
             mouse_rect.y = mouse_pos.y;
             if(CheckCollisionRecs(it->rect, mouse_rect)) {
                 if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-                    it->active = true;
-                    it->type = curr_place_type;
+                    if(it->type != CELL_TYPE_ROCK) it->type = curr_place_type;
                 } else if(IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
-                    it->active = false;
+                    if(it->type != CELL_TYPE_ROCK) it->type = CELL_TYPE_NONE;
+                } else if(IsMouseButtonDown(MOUSE_MIDDLE_BUTTON)) {
+                    if(it->type != CELL_TYPE_ROCK) it->type = CELL_TYPE_WATER;
                 } else {
                     it->selected = true;
                 }
             } else {
                 it->selected = false;
             }
+            it->updated = false;
         }
 
         BeginDrawing();
@@ -108,10 +126,16 @@ int main(void) {
             Color c = it->color;
             if (it->selected) {
                 c = ColorBrightness(RED, 0.3);
-            } else if(it->active) {
+            } else {
                 switch (it->type) {
                     case CELL_TYPE_SAND: {
                         c = BEIGE;
+                    } break;
+                    case CELL_TYPE_WATER: {
+                        c = BLUE;
+                    } break;
+                    case CELL_TYPE_ROCK: {
+                        c = DARKGRAY;
                     } break;
                     default: break;
                 }
@@ -133,13 +157,48 @@ int main(void) {
 }
 
 void UpdateSand(Grid*grid, Cell*c, int col, int row) {
-    int i = GetRandomValue(-1, 1);
-    int next = (col+i) + (row+1) * GRID_SIZE;
-    if(next < grid->count) {
-        if(!grid->items[next].active) {
-            c->active = false;
-            grid->items[next].active = true;
-            grid->items[next].type = CELL_TYPE_SAND;
+    if(c->updated) return;
+    c->updated = true;
+
+    size_t down = col + (row+1) * GRID_SIZE;
+    if(UpdateCell(grid, c, CELL_TYPE_NONE,CELL_TYPE_NONE, CELL_TYPE_SAND, down)) return;
+
+    size_t down_left = (col-1) + (row+1) * GRID_SIZE;
+    if(UpdateCell(grid, c, CELL_TYPE_NONE,CELL_TYPE_NONE, CELL_TYPE_SAND, down_left)) return;
+
+    size_t down_right = (col+1) + (row+1) * GRID_SIZE;
+    if(UpdateCell(grid, c, CELL_TYPE_NONE,CELL_TYPE_NONE, CELL_TYPE_SAND, down_right)) return;
+}
+
+bool UpdateCell(Grid* grid, Cell* c, Cell_Type old_type, Cell_Type new_type, Cell_Type next_type, size_t next_pos) {
+    if(next_pos < grid->count) {
+        if(grid->items[next_pos].type == old_type) {
+            c->type = new_type;
+            grid->items[next_pos].type = next_type;
+            grid->items[next_pos].updated = true;
+            return true;
         }
     }
+    return false;
+}
+
+void UpdateWater(Grid*grid, Cell*c, int col, int row) {
+    if(c->updated) return;
+    c->updated = true;
+
+    size_t down = col + (row+1) * GRID_SIZE;
+    if(UpdateCell(grid, c, CELL_TYPE_NONE, CELL_TYPE_NONE, CELL_TYPE_WATER, down)) return;
+
+    size_t down_left = (col-1) + (row+1) * GRID_SIZE;
+    if(UpdateCell(grid, c, CELL_TYPE_NONE, CELL_TYPE_NONE, CELL_TYPE_WATER, down_left)) return;
+
+    size_t down_right = (col+1) + (row+1) * GRID_SIZE;
+    if(UpdateCell(grid, c, CELL_TYPE_NONE, CELL_TYPE_NONE, CELL_TYPE_WATER, down_right)) return;
+
+    size_t left = (col-1) + row * GRID_SIZE;
+    if(UpdateCell(grid, c, CELL_TYPE_NONE, CELL_TYPE_NONE, CELL_TYPE_WATER, left)) return;
+
+    size_t right = (col+1) + row * GRID_SIZE;
+    if(UpdateCell(grid, c, CELL_TYPE_NONE, CELL_TYPE_NONE, CELL_TYPE_WATER, right)) return;
+
 }
--- nob.c
+++ nob.c
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
     nob_cc_inputs(&cmd, "game.c");
     nob_cc_output(&cmd, "build/game");
     nob_cc_flags(&cmd);
-    cmd_append(&cmd, "-ggdb", "-lraylib", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lX11");
+    cmd_append(&cmd, "-lraylib", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lX11");
     if(!cmd_run_sync_and_reset(&cmd)) return 1;
 
     if(argc <= 1) return 0;
