32ab5c7ae72e47370ead940533c48a4959e141ff
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-08-20 10:36:54 -0300

    Extract build logic into separate function and add rebuild check

--- nob.c
+++ nob.c
@@ -2,25 +2,36 @@
 #define NOB_STRIP_PREFIX
 #include "nob.h"
 
+#define BUILD_DIR "build"
+#define BIN_PATH BUILD_DIR"/game"
+#define SRC_PATH "game.c"
+
+bool build_game(Cmd*cmd) {
+    nob_cc(cmd);
+    nob_cc_inputs(cmd, SRC_PATH);
+    nob_cc_output(cmd, BIN_PATH);
+    nob_cc_flags(cmd);
+    cmd_append(cmd, "-lraylib", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lX11");
+    return cmd_run_sync_and_reset(cmd);
+}
+
 int main(int argc, char** argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
-
-    if(!mkdir_if_not_exists("build")) return 1;
-
     Cmd cmd = {0};
-    nob_cc(&cmd);
-    nob_cc_inputs(&cmd, "game.c");
-    nob_cc_output(&cmd, "build/game");
-    nob_cc_flags(&cmd);
-    cmd_append(&cmd, "-lraylib", "-lGL", "-lm", "-lpthread", "-ldl", "-lrt", "-lX11");
-    if(!cmd_run_sync_and_reset(&cmd)) return 1;
+
+    if(!mkdir_if_not_exists(BUILD_DIR)) return 1;
+    if(file_exists(BIN_PATH)) {
+        if(nob_needs_rebuild1(BIN_PATH, SRC_PATH)) {
+            if(!build_game(&cmd)) return 1;
+        }
+    }
 
     if(argc <= 1) return 0;
 
     const char* arg = shift(argv, argc);
 
     if(strcmp(arg, "run")) {
-        cmd_append(&cmd, "build/game");
+        cmd_append(&cmd, BIN_PATH);
         if(!cmd_run_sync_and_reset(&cmd)) return 1;
     }
 
